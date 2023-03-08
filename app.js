b94ee10e9c1a2bca5487ca253f7548ac742174a2
miki <milkyas.dev@gmail.com>
2023-03-08 21:05:51 +0300

    added qrcode generation functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const qr = document.getElementById('qrcode');
 
 const onGenerateSubmit = (e) => {
     e.preventDefault()
+    celarUI()
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
 
@@ -15,18 +16,38 @@ const onGenerateSubmit = (e) => {
         showSpinner()
         setTimeout(() => {
             hideSpinner()
+            generateQRCode(url, size)
         }, 1000);
     }
 }
 
+
+const generateQRCode = (url, size) => {
+    document.getElementById('image').style.display = 'none';
+    document.getElementById('qrcodeContainer').style.display = 'flex';
+
+
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+    })
+
+}
+
 const showSpinner = () => {
     document.getElementById('spinner').style.display = 'grid';
+    document.getElementById('qrcodeContainer').style.display = 'none';
     document.getElementById('image').style.display = 'none';
 }
 
 const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none';
     document.getElementById('image').style.display = 'block';
+}
+
+const celarUI = () => {
+    qr.innerHTML = ''
 }
 
 hideSpinner();
